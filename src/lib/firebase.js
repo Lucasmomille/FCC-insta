9f2309b5758846aebc5567a520cd66bf2a1e1d61
Lucas <lucas.moquet95@gmail.com>
2021-05-06 17:50:53 +0200

    tailwind ok

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,9 +2,22 @@ import Firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const config = {};
+//import { seedDatabase } from '../seed'; We don't have to run this twice
+
+
+const config = {
+    apiKey: "AIzaSyBwBVzNqUyR12jsaPFHpeuRqIHxRJxWyro",
+    authDomain: "insta-like-e3c70.firebaseapp.com",
+    projectId: "insta-like-e3c70",
+    storageBucket: "insta-like-e3c70.appspot.com",
+    messagingSenderId: "904855237024",
+    appId: "1:904855237024:web:cc3a1b7719f8b0e188f4c5"
+
+};
 
 const firebase = Firebase.initializeApp(config);
 const { FieldValue } = Firebase.firestore;
+
+//seedDatabase(firebase);
 
 export { firebase, FieldValue };
---
+++ src/pages/login.js
@@ -0,0 +1,26 @@
+/* import { useState, useContext, useEffect } from 'react';
+// eslint-disable-next-line
+import { useParams, useHistory } from "react-router";
+import FirebaseContext from '../context/firebase'; */
+
+export default function Login() {
+    /* const history = useHistory();
+    const { firebase } = useContext(FirebaseContext);
+
+    const [emailAddress, setEmailAddress] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [error, setError] = useState();
+    const isInvalid = password === '' || emailAddress === '';
+
+    const handleLogin = () => { };
+
+    useEffect(() => {
+        document.title = 'Login -Instagram';
+    }, []) */
+
+
+    return (
+        <div className="container flex mx-auto max-w-screen-md items-center bg-blue-500">Test</div>
+    )
+}
