dd4c08adf8b434e9e326df1dd5452a06ccda5bfe
Lucas <lucas.moquet95@gmail.com>
2021-05-14 16:17:26 +0200

    login and signup ok

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,6 +1,6 @@
 export const DASHBOARD = '/';
 export const LOGIN = '/login';
-export const SIGNUP = '/signup';
+export const SIGNUP = '/sign-up';
 export const PROFIL = '/p/:username';
 export const NOT_FOUND = '/not-found';
 
